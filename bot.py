168abc4e854422d007cb8851d3a01b8e538d03ff
Schtirlitz80 <Schtirlitz80@mail.ru>
2023-02-14 14:46:35 +0300

    New bot options (old or new notices)

--- bot.py
+++ bot.py
@@ -5,10 +5,9 @@
 
 from config import API_TOKEN
 from aiogram import Bot, executor, Dispatcher, types
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
 
 from av_by import get_car_info_list
-
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
@@ -16,6 +15,11 @@ dp = Dispatcher(bot)
 kb = ReplyKeyboardMarkup(resize_keyboard=True)
 b1 = KeyboardButton(text='Rapid на av.by')
 kb.add(b1)
+
+ikb = InlineKeyboardMarkup()
+ib1 = InlineKeyboardButton(text='Все объявления', callback_data='av_by_all')
+ib2 = InlineKeyboardButton(text='Только новые', callback_data='av_by_new')
+ikb.add(ib1, ib2)
 
 
 @dp.message_handler(commands=['start'])
@@ -28,8 +32,15 @@ async def cmd_start(message: types.Message):
 
 @dp.message_handler(lambda message: message.text == 'Rapid на av.by')
 async def av_by(message: types.Message):
-    await message.reply('Данные с сайта av.by:\nПолучаю данные. Подождите...')
+    await message.reply('Автомобили Skoda Rapid I начиная с 2014 года на вторичном рынке Республики Беларусь с сайта av.by. Какие объявления показать?',
+                        reply_markup=ikb)
 
+
+@dp.callback_query_handler()
+async def av_by_all(callback: types.CallbackQuery):
+    if callback.data != 'av_by_all':
+        return
+    await callback.answer('Данные с сайта av.by:\nПолучаю данные. Подождите...')
     try:
         car_list = get_car_info_list()
         print(len(car_list))
@@ -51,15 +62,15 @@ async def av_by(message: types.Message):
             photo_caption = f'{car_name}\n{car_year}\n{car_description}\n{km}\n{price} ({price_usd})\n{car_url}\n{location}'
 
             if car_img != "No image":
-                await bot.send_photo(chat_id=message.chat.id,
+                await bot.send_photo(chat_id=callback.from_user.id,
                                      photo=car_img,
                                      caption=photo_caption)
             else:
-                await message.answer(photo_caption)
+                await callback.answer(photo_caption)
 
     except Exception as _ex:
         print(_ex)
-        await message.answer('Не могу получить данные с сайта av.by...')
+        await callback.answer('Не могу получить данные с сайта av.by...')
 
 
 
