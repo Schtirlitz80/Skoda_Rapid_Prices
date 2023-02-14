4aa891b0462a68194290bca95b882ae47b3e669c
Schtirlitz80 <Schtirlitz80@mail.ru>
2023-02-14 10:18:19 +0300

    Working database.py module

--- database.py
+++ database.py
@@ -1,11 +1,9 @@
 import sqlite3
 
 
-with open('cars_av_by.db', 'w', encoding='UTF-8') as file:
-    db = sqlite3.connect(file)
-
+db = sqlite3.connect('cars_av_by.db')
+with db:
     cursor = db.cursor()
-
     cursor.execute(
         """CREATE TABLE IF NOT EXISTS av_cars(
                         id INTEGER PRIMARY KEY,
@@ -22,19 +20,55 @@ with open('cars_av_by.db', 'w', encoding='UTF-8') as file:
     )
 
 
-'''
-car_name = car["car"]
+def put_list_into_db(lst: list) -> list:
+    """
+    Получает список машин, пытается добавить их в базу данных.
+    Если уникальный url объявления уже есть в базе, то запись не добавляется.
+    Возвращает список машин, которых не было в базе и которые были добавлены
+    :param lst:
+    :return:
+    """
+    d_base = sqlite3.connect('cars_av_by.db')
+    with d_base:
+        cur = d_base.cursor()
+        cur.execute(
+            """CREATE TABLE IF NOT EXISTS av_cars(
+                                        id INTEGER PRIMARY KEY,
+                                        car_name TEXT,
+                                        car_img TEXT,
+                                        car_year TEXT,
+                                        car_description TEXT,
+                                        km TEXT,
+                                        price TEXT,
+                                        price_usd TEXT,
+                                        car_url TEXT unique,
+                                        location TEXT
+                        )"""
+        )
 
-car_img = car["photo"]
-print(car_img)
+        new_cars_lst = []
 
-year = car["year"]
-car_description = car["descriprion"]
-km = car["km"]
-price = car["price"]
-price_usd = car["usd_price"]
-car_url = car["url"]
-location = car["location"]
+        for car in lst:
+            car_name = car["car"]
+            car_img = car["photo"]
+            car_year = car["year"]
+            car_description = car["description"]
+            km = car["km"]
+            price = car["price"]
+            price_usd = car["usd_price"]
+            car_url = car["url"]
+            location = car["location"]
 
-photo_caption = f'{car_name}\n{year}\n{car_description}\n{km}\n{price} ({price_usd})\n{car_url}\n{location}'
-'''
+            # ids = cursor.execute('SELECT user_id FROM users WHERE user_id = ?', (message.from_user.id,))
+            # использовать PREPAIRED MESSAGE
+            sql_query = f"""INSERT INTO av_cars(car_name, car_img, car_year, car_description, km, price, price_usd, car_url, location)
+                                VALUES ('{car_name}', '{car_img}', '{car_year}', '{car_description}', '{km}', '{price}', '{price_usd}', '{car_url}', '{location}')
+                        """
+            try:
+                cur.execute(sql_query)
+                new_cars_lst.append(car)
+            except Exception as _ex:
+                print(_ex)
+
+        d_base.commit()
+    return new_cars_lst
